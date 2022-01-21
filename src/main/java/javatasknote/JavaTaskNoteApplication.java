e92ba774c74ec66b74d6b954433382c66b0a590a
Daniil Palii <palii.daniil@protonmail.com>
2022-01-22 00:35:46 +0100

    Generate Spring project

---
+++ src/main/java/javatasknote/JavaTaskNoteApplication.java
@@ -0,0 +1,11 @@
+package javatasknote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTaskNoteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTaskNoteApplication.class, args);
+    }
+}
---
+++ src/test/java/javatasknote/JavaTaskNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package javatasknote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTaskNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
