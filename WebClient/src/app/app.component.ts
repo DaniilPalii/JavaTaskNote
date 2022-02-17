de1adf05f359c6209fb0a3dcf090981940d11c07
Daniil Palii <palii.daniil@protonmail.com>
2022-02-17 19:31:03 +0100

    Add example data fetching

--- WebClient/src/app/app.component.ts
+++ WebClient/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'task-note';
+export class AppComponent implements OnInit {
+  public title = 'Hello, Angular!';
+  public response = 'loading...';
+
+  public constructor(private httpClient: HttpClient) {
+  }
+
+  public ngOnInit(): void {
+    this.httpClient.get('/api/greeting', { responseType: "text" })
+      .subscribe(response => this.response = response);
+  }
 }
+
